683c178f26e684953601c7a02cce4e0af79289c0
Anatolii <bugrovka12@ukr.net>
2019-04-21 19:55:25 +0300

    Fix path to site

--- aperture.py
+++ aperture.py
@@ -1,7 +1,7 @@
 import sys
 import logging
 logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/var/www/html/devopscamp-pythonsite/")
+sys.path.insert(0,"/var/www/html/app/")
 
 from Aperture import app as application
 application.secret_key = 'Add your secret key'
